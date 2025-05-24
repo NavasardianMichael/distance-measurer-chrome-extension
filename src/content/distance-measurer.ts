7d09c83142a2875ce9ea9988b88de7201872e3ce
NavasardianMichael <navasardianmichael@gmail.com>
2025-05-25 03:31:14 +0400

    modal content being generated

--- src/content/distance-measurer.ts
+++ src/content/distance-measurer.ts
@@ -5,6 +5,10 @@ const hoveredClassName = styles['distance-measurer-extension_hovered']
 const selectedClassName = styles['distance-measurer-extension_selected']
 const lineWidth = 2
 
+const state = {
+  isMoreInfoModalOpen: false,
+}
+
 const generateArrow = (
   frameLeft: number,
   frameRight: number,
@@ -86,19 +90,23 @@ const constructMetrics = (elementsSet: Set<HTMLElement>) => {
   const frameRight = Math.max(firstElementLeft, secondElementLeft)
 
   const metricsContainer = document.createElement('div')
+  metricsContainer.classList.add(styles.metricsContainer)
+
+  // Painting Frames
+  const framesFragment = document.createDocumentFragment()
 
   if (!isVerticallyOnSameLine) {
     const frameTopBorder = document.createElement('div')
     frameTopBorder.classList.add(styles.frameBorder)
     frameTopBorder.classList.add(styles.frameHorizontalBorder)
     frameTopBorder.style.top = `${frameTop}px`
-    metricsContainer.appendChild(frameTopBorder)
+    framesFragment.appendChild(frameTopBorder)
 
     const frameBottomBorder = document.createElement('div')
     frameBottomBorder.classList.add(styles.frameBorder)
     frameBottomBorder.classList.add(styles.frameHorizontalBorder)
     frameBottomBorder.style.top = `${frameBottom}px`
-    metricsContainer.appendChild(frameBottomBorder)
+    framesFragment.appendChild(frameBottomBorder)
 
     const verticalArrowMetric = generateArrow(
       frameLeft + window.scrollX,
@@ -109,8 +117,9 @@ const constructMetrics = (elementsSet: Set<HTMLElement>) => {
         isVertical: true,
       }
     )
-    metricsContainer.appendChild(verticalArrowMetric)
+    framesFragment.appendChild(verticalArrowMetric)
   }
+  metricsContainer.appendChild(framesFragment)
 
   if (!isHorizontallyOnSameLine) {
     const frameLeftBorder = document.createElement('div')
@@ -137,6 +146,7 @@ const constructMetrics = (elementsSet: Set<HTMLElement>) => {
     metricsContainer.appendChild(horizontalArrowMetric)
   }
 
+  // Painting Modal
   const moreInfoModalContainer = document.createElement('div')
   moreInfoModalContainer.classList.add(styles.moreInfoModalContainer)
   const moreInfoTriggerBtn = document.createElement('button')
@@ -144,21 +154,71 @@ const constructMetrics = (elementsSet: Set<HTMLElement>) => {
   moreInfoTriggerBtn.style.left = `${frameLeft + window.scrollX}px`
   moreInfoTriggerBtn.style.top = `${frameTop + window.scrollY}px`
   moreInfoTriggerBtn.title = 'More info'
+  moreInfoTriggerBtn.innerHTML = '&#9432;'
   moreInfoTriggerBtn.onclick = (e) => {
+    console.log('moreInfoTriggerBtn', { e })
     e.stopPropagation()
+
+    const moreInfoModalContentContainer = document.createElement('div')
+    moreInfoModalContentContainer.classList.add(styles.moreInfoModalContentContainer)
+
     const moreInfoModalContent = document.createElement('div')
     moreInfoModalContent.classList.add(styles.moreInfoModalContent)
-    moreInfoModalContent.innerText = 'More info about the selected elements'
-    moreInfoModalContainer.appendChild(moreInfoModalContent)
+    moreInfoModalContent.innerHTML = `
+    <div style="margin-bottom: 8px;"><strong>Vertical distances</strong></div>
+    <ul class="${styles.moreInfoList}" style="margin-bottom: 16px;">
+      <li>
+        <svg width="24" height="24" style="vertical-align:middle;margin-right:4px;">
+          <line x1="12" y1="2" x2="12" y2="22" stroke="#0078D4" stroke-width="2"/>
+          <polygon points="12,2 8,8 16,8" fill="#0078D4"/>
+          <polygon points="12,22 8,16 16,16" fill="#0078D4"/>
+        </svg>
+        From Top of the top element to the top of the bottom element: <strong>${+Math.abs(firstElementTop - secondElementTop).toFixed(2)}px</strong>
+      </li>
+      <li>
+        <svg width="24" height="24" style="vertical-align:middle;margin-right:4px;">
+          <line x1="12" y1="2" x2="12" y2="22" stroke="#E81123" stroke-width="2"/>
+          <polygon points="12,22 8,16 16,16" fill="#E81123"/>
+          <polygon points="12,2 8,8 16,8" fill="#E81123"/>
+        </svg>
+        From Bottom of the top element to the bottom of the bottom element: <strong>${+Math.abs(firstElementBottom - secondElementBottom).toFixed(2)}px</strong>
+      </li>
+    </ul>
+    <div style="margin-bottom: 8px;"><strong>Horizontal distances</strong></div>
+    <ul class="${styles.moreInfoList}">
+      <li>
+        <svg width="24" height="24" style="vertical-align:middle;margin-right:4px;">
+          <line x1="2" y1="12" x2="22" y2="12" stroke="#107C10" stroke-width="2"/>
+          <polygon points="2,12 8,8 8,16" fill="#107C10"/>
+          <polygon points="22,12 16,8 16,16" fill="#107C10"/>
+        </svg>
+        From Left of the left element to the left of the right element: <strong>${+Math.abs(firstElementLeft - secondElementLeft).toFixed(2)}px</strong>
+      </li>
+      <li>
+        <svg width="24" height="24" style="vertical-align:middle;margin-right:4px;">
+          <line x1="2" y1="12" x2="22" y2="12" stroke="#B4009E" stroke-width="2"/>
+          <polygon points="22,12 16,8 16,16" fill="#B4009E"/>
+          <polygon points="2,12 8,8 8,16" fill="#B4009E"/>
+        </svg>
+        From Right of the left element to the right of the right element: <strong>${+Math.abs(firstElementRight - secondElementRight).toFixed(2)}px</strong>
+      </li>
+    </ul>
+    `
+    moreInfoModalContentContainer.appendChild(moreInfoModalContent)
+    moreInfoModalContainer.appendChild(moreInfoModalContentContainer)
 
     const moreInfoModalOverlay = document.createElement('div')
     moreInfoModalOverlay.classList.add(styles.moreInfoModalOverlay)
-    moreInfoModalContainer.appendChild(moreInfoModalOverlay)
+    moreInfoModalContentContainer.appendChild(moreInfoModalOverlay)
     moreInfoModalOverlay.onclick = (e) => {
+      console.log('moreInfoModalOverlay', { e })
+
       e.stopPropagation()
       metricsContainer.removeChild(moreInfoModalContainer)
+      state.isMoreInfoModalOpen = false
     }
     metricsContainer.appendChild(moreInfoModalContainer)
+    state.isMoreInfoModalOpen = true
   }
 
   moreInfoModalContainer.appendChild(moreInfoTriggerBtn)
@@ -218,8 +278,10 @@ export const initDistanceMeasurer = (app: HTMLDivElement) => {
   })
 
   document.addEventListener('click', (e) => {
+    console.log('click', { e, isCtrlPressed })
     e.preventDefault()
     e.stopPropagation()
+    if (state.isMoreInfoModalOpen) return
     const clickedElement = e.target as HTMLElement
 
     if (!isCtrlPressed) {
